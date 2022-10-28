82fd4b5cfa9edb63e9b4d80b780531c0902d15e2
GelcimarMoraes <60938131+GelcimarMoraes@users.noreply.github.com>
2022-10-28 17:34:29 -0300

    att03

---
+++ 2_TIPOS_DE_DADOS_E_OPERADORES/7_boolean_comparacao/script.js
@@ -0,0 +1,10 @@
+console.log(1 > 2);
+console.log(5 < 10);
+console.log(3 >= 3);
+console.log(5 <= 4);
+console.log(5 == 4);
+console.log('Gelcimar' != 'Gelcimar');
+console.log(3 === '3');
+console.log(4 === 4);
+
+
