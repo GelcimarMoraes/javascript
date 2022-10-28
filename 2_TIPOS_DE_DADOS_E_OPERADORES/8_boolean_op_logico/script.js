7551d48648028273be9a110f2056ade4766c7068
GelcimarMoraes <60938131+GelcimarMoraes@users.noreply.github.com>
2022-10-28 17:58:28 -0300

    att04

---
+++ 2_TIPOS_DE_DADOS_E_OPERADORES/8_boolean_op_logico/script.js
@@ -0,0 +1,12 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+
+console.log(3 == 3 && "Gelcimar" == "Gelcimar");
+console.log("felipe" == "joh" || false);
+
+console.log(!(true && true));
+
+console.log(true && true);
+console.log(false || false);
+
+
---
+++ 2_TIPOS_DE_DADOS_E_OPERADORES/9_boolean_op_ternario/scripts.js
@@ -0,0 +1,4 @@
+console.log(5 > 2 ? 'É sim' : 'É não');
+console.log(false ? 5 : 4);
+console.log("Gelcimar" == "Gelcimar" ? "Olá, Gelcimar" : "Não é o Gelcimar");
+
