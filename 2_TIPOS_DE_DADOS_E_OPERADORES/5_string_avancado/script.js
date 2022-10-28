8c620b93a5160be90e26badce6c139d7de7373a6
GelcimarMoraes <60938131+GelcimarMoraes@users.noreply.github.com>
2022-10-28 17:16:42 -0300

    att02

---
+++ 2_TIPOS_DE_DADOS_E_OPERADORES/5_string_avancado/script.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \n Segunda linha");
+console.log("O meu nome é 'Gelcimar'");
+console.log('O nome dela é "Roberta"')
+console.log(`A multiplicação de 5 por 3 é igual á: ${5*3}`);
+console.log("O " + "meu " + "nome " + "é " + "Gelcimar.")
