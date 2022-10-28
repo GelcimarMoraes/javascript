80ba01f32b1ea2279896826f1a4b3cdbe18e29dd
GelcimarMoraes <60938131+GelcimarMoraes@users.noreply.github.com>
2022-10-28 16:53:20 -0300

    att

---
+++ 2_TIPOS_DE_DADOS_E_OPERADORES/4_string/script.js
@@ -0,0 +1,6 @@
+console.log(typeof "Oi, meu nome é Gelcimar");
+console.log(typeof 'Eu queria comprar um carro.');
+console.log(typeof 'teste');
+console.log(typeof "infinity");
+console.log(typeof Infinity);
+
