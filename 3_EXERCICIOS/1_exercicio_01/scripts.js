cc311ea8e6a2e736ae6d37de7c2031c604ff8b30
GelcimarMoraes <60938131+GelcimarMoraes@users.noreply.github.com>
2022-10-29 18:27:26 -0300

    att06

---
+++ 3_EXERCICIOS/1_exercicio_01/scripts.js
@@ -0,0 +1,3 @@
+console.log("Hello World!");
+console.log('Meu nome é Gelcimar');
+console.log(`Este aqui é o template literals`);
---
+++ 3_EXERCICIOS/3_exercicio_03/scripts.js
@@ -0,0 +1,3 @@
+console.log(12 > 5);
+console.log(4 <= 4);
+console.log("Gelcimar" != "Pedro");
